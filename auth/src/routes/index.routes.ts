af9c2e3ff641407cbacb37390cd57cad1eab35c4
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-11-19 22:43:53 -0300

    [MOD] Se Agrega not found error

--- auth/src/routes/index.routes.ts
+++ auth/src/routes/index.routes.ts
@@ -1,6 +1,8 @@
 import express = require('express');
+import 'express-async-errors';
 import {currentUserRouter} from './current-user.routes';
 import {signRouter} from './sign.routes';
+import { NotFoundError } from "../errors/index-error";
 
 const app = express();
 
@@ -9,6 +11,11 @@ app.use(currentUserRouter);
 
 // Ruta para login
 app.use(signRouter);
+
+app.all('*', async(req, res, next) => {
+  // next (new NotFoundError());
+  throw new NotFoundError();
+}); 
  
 export {
   app as routes 
