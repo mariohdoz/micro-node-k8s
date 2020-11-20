4ed2b0425750c1097b306e8c847c764efe8b7975
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-11-19 22:43:01 -0300

    [MOD] Se modifica manejo de errores

---
+++ auth/src/errors/index-error.ts
@@ -0,0 +1,6 @@
+import { CustomError } from "./custom-error";
+import { DatabaseConnectionError } from "./database-connection-error";
+import { NotFoundError } from './not-found-error';
+import { RequestValidationError } from "./request-validation-error";
+
+export {CustomError, DatabaseConnectionError, NotFoundError, RequestValidationError};
