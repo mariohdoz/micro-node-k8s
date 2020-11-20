05cc8557ea3391e246fe372229ecec2ff678d856
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-11-19 22:43:25 -0300

    [MOD] Se modifica manejo de errores

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,28 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from  '../errors/request-validation-error';
-import { DatabaseConnectionError } from  '../errors/database-connection-error';
+import { CustomError } from  '../errors/custom-error';
 
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
   const aux_err = err;
 
-  if (err instanceof RequestValidationError ) {
-
-    const formattedErros = err.errors.map(error => {
-      return {
-        message: error.msg,
-        field: error.param
-      };
-    }) 
-
-    return res.status(400).send({errorssss: formattedErros});
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    
-    return res.status(500).send({errors: [{message: err.reason}] });
-
+  if (err instanceof CustomError ) {
+    return res.status(err.statusCode).send({errors: err.serializeErrors()});
   }
 
   res.status(400)
