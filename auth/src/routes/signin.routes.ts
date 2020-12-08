ba2088459aa0c37ea7178955d35b5814082911e9
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-12-08 18:52:59 -0300

    [MOD] Limpieza archivos

--- auth/src/routes/signin.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as authController from '../controllers/auth.controller';
-
-const router = Router();
-
-router.get('/api/users/signin', authController.postsignIn);
-
-export {router as signInRouter};
--- auth/src/routes/signup.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as authController from '../controllers/auth.controller';
-import { userValidationResult, validate } from '../middlewares/validator'
-
-const router = Router();
-
-router.post('/api/users/signup', userValidationResult(), validate, authController.postsignUp);
-
-export {router as signUpRouter};
