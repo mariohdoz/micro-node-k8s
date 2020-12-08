e6ec8d9f9db6960d62bffab45bce582eebe7f973
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-12-08 17:48:03 -0300

    [MOD] Se elimina ciertos parametros en las respuestas de usuario

--- auth/src/models/user.models.ts
+++ auth/src/models/user.models.ts
@@ -30,19 +30,29 @@ interface UserDoc extends mongoose.Document {
 }
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
+  {
+    toJSON: {
+      transform(doc, ret){
+        delete ret.password;
+        delete ret._id;
+        delete ret.__v;
+      }
+    }
   }
-});
+);
 
 userSchema.pre('save', async function (this: UserDoc, next: any) {
   if (this.isModified('password')) {
