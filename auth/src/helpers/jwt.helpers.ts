d7c44fb7b6b51e2703688a3e83b6d840c7bb9668
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-12-11 23:48:10 -0300

    [ADD] Se crea función para verificar token

--- auth/src/helpers/jwt.helpers.ts
+++ auth/src/helpers/jwt.helpers.ts
@@ -1,8 +1,27 @@
 import * as jwt from 'jsonwebtoken';
 import config from "../config/index";
 
+/**
+ * Generate token
+ * @param data array with user data
+ * @return user token
+ */
 const generateToken = (data: {email: string, id: string}) => {
   return jwt.sign(data, config.JWT_KEY);
 }  
 
-export { generateToken }
+/**
+ * 
+ * @param token token from user
+ * @param key JWT key
+ * @return payload
+ */
+const verify = (token: string, key: string) => {
+  try {
+    return jwt.verify(token, key);
+  } catch (error) {
+    return null;
+  }
+}
+
+export { generateToken, verify }
