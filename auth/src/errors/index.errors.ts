9f25d85890629e8d080d9a05cf70301cc6937d42
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-12-12 00:42:03 -0300

    [MOD] Refactor a import de middlewate

--- auth/src/errors/index.errors.ts
+++ auth/src/errors/index.errors.ts
@@ -2,6 +2,14 @@ import { CustomError } from "./custom-error.errors";
 import { DatabaseConnectionError } from "./database-connection-error.errors";
 import { NotFoundError } from "./not-found-error.errors";
 import { RequestValidationError } from "./request-validation-error.errors";
-import { BadRequestError } from "./bad-request.errors"
+import { BadRequestError } from "./bad-request.errors";
+import { NotAuthorizedError } from "./not-authorize.errors"; 
 
-export {CustomError, DatabaseConnectionError, NotFoundError, RequestValidationError, BadRequestError};
+export {
+  CustomError,
+  DatabaseConnectionError,
+  NotFoundError,
+  RequestValidationError,
+  BadRequestError,
+  NotAuthorizedError
+};
--- auth/src/routes/sign.routes.ts
+++ auth/src/routes/sign.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import { authController } from "../controllers/index.controller";
-import { CreateUserValidationResult, LoginUserValidationResult, validate } from '../middlewares/index.middlewares'; 
+import { authController } from "../controllers/index.controllers";
+import { validator } from '../middlewares/index.middlewares'; 
 
 const router = Router();
 
 router.post('/api/users/signout', authController.postsignOut)
 
-router.post('/api/users/signin', LoginUserValidationResult(), validate, authController.postsignIn);
+router.post('/api/users/signin', validator.LoginUserValidationResult(), validator.validate, authController.postsignIn);
 
-router.post('/api/users/signup', CreateUserValidationResult(), validate, authController.postsignUp);
+router.post('/api/users/signup', validator.CreateUserValidationResult(), validator.validate, authController.postsignUp);
 
 export {router as signRouter};
