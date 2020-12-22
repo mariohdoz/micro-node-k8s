ce1d1465e9b5b7349359965b01233d43eb753d77
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-12-21 23:08:18 -0300

    [ADD] Se agrega page de signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -5,48 +5,67 @@ const signup = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [errors, setErrors] = useState([]);
 
     const onSubmit = async (event) => {
         event.preventDefault();
+        
+        try {            
+            const response = await axios.post('/api/users/signup', {
+              email,
+              password,
+              name,
+            });
 
-        const response = await axios.post('/api/users/signup', {
-            email, 
-            password
-        });
+            console.log('response');
+            console.log(response);
+            console.log('response.data');
+            console.log(response.data);
+        } catch (error) {
+            console.log(error.response.data.errors)
+            setErrors(error.response.data.errors);
+        }
 
-        console.log(response.data);
     };
 
     return (
-        <form onSubmit={onSubmit}> 
-            <h1>
-                Sign Up
-            </h1>
-            <div className="form-group">
-                <label>
-                    Email Address
-                </label>
-                <input 
-                    value={email} 
-                    onChange={e => setEmail(e.target.value)} 
-                    className="form-control"
-                />
-            </div>
-            <div className="form-group">
-                <label>
-                    Password
-                </label>
-                <input 
-                    type="password" 
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    className="form-control"
-                />
-            </div>
-            <button className="btn btn-primary">
-                Sign Up
-            </button>
-        </form>
+      <form onSubmit={onSubmit}>
+        <h1>Sign Up</h1>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Full Name</label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        { errors.length > 0 && <div className="alert alert-danger">
+          {errors.map((err) => (
+              <ul className="my-0">
+                  <li key={err.message}>{err.message}</li>
+              </ul>
+          ))}
+        </div>}
+        <button className="btn btn-primary">Sign Up</button>
+      </form>
     );
 };
 
