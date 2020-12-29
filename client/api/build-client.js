1c6b2d81f26a3cce866a0811a8bbc90f0f036970
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-12-28 23:35:24 -0300

    [ADD] Se crea build client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 const buildClient = ({ req }) => {
 
-  console.log(typeof window);
+  // console.log(typeof window);
   if (typeof window === 'undefined') {
     // We are on the server
-    console.log('Entro RSS');
+    // console.log('Entro RSS');
     return axios.create({
       baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
       headers: req.headers,
     });
   } else {
-    console.log('Entro local');
+    // console.log('Entro local');
     // We must be on the browser
     return axios.create({
       baseURL: '/',
