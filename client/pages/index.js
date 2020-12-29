0a4dc0d0de9b02cc275e1bddd5ecbe6842a7e120
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-12-28 23:36:07 -0300

    [MOD] Se actualiza index

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,20 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  return currentUser ? (
+    <div>You're signed in</div>
+  ) : (
+    <div>You're not signed in</div>
+  )
 };
+
+LandingPage.getInitialProps = async context => {
+
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
