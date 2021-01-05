eef317b51245027ca33f6e5e39407a2e102bf97a
Mario Ossandón Z <mario.hdoz1@gmail.com>
2021-01-04 23:09:27 -0300

    [MOD]actualización env auth

--- auth/src/server/start.ts
+++ auth/src/server/start.ts
@@ -4,6 +4,14 @@ import config from "../config/index";
 
 const start = async () => {
 
+  if(!config.JWT_KEY){
+    throw new Error('JWT_KEY debe estar definida');
+  }
+
+  if(!config.MONGO_URI){
+    throw new Error('MONGO_URI debe estar definida');
+  }
+
   try {
     await mongoose.connect(config.MONGO_URI , {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
     console.log('Base de datos conectada')
