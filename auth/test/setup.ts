edf9b890b5cfd67af065ccce545201e8691131aa
Mario Ossandón Z <mario.hdoz1@gmail.com>
2021-01-17 21:57:16 -0300

    [MOD] corrección en uso de commons

--- auth/test/setup.ts
+++ auth/test/setup.ts
@@ -1,24 +1,34 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-// import { app } from "../src/server/server";
+import request from "supertest";
+import { app } from "../src/server/server";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
 
   process.env.JWT_KEY = 'testkey';
-  
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
   await mongoose.connect(mongoUri, {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
-  })
 
+  });
 });
 
 beforeEach( async () => {
-  const collections  =  await mongoose.connection.db.collections();
+  const collections = await mongoose.connection.db.collections();
 
   collections.forEach((collection) => {
     collection.deleteMany({});
@@ -30,3 +40,20 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
