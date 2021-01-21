691fd2a0025095163dc84873b7fc3e2ce7e5cf16
Mario Ossandón Z <mario.hdoz1@gmail.com>
2021-01-20 23:31:32 -0300

    [MOD] Se importa currentUser

--- tickets/src/server/server.ts
+++ tickets/src/server/server.ts
@@ -5,19 +5,21 @@ import * as bodyParser from 'body-parser';
 import cookieSession from "cookie-session";
 
 import { routes } from '../routes/index.routes';
-import { errorHandler } from '@hdozdev/common';
+import { errorHandler, currentUser } from '@hdozdev/common';
+
 
 app.set('trust proxy', true);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
-  cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== 'test'
-  }
-));
+    cookieSession({
+      signed: false,
+      secure: process.env.NODE_ENV !== 'test'
+    }
+  ));
 
+app.use(currentUser);
 app.use(routes);
 app.use(errorHandler);
 
