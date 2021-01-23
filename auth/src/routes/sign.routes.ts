c7021934cc1dccc4bcbb0374d81aee1b4d76a0f1
Mario Ossandón Z <mario.hdoz1@gmail.com>
2021-01-23 20:06:45 -0300

    [MOD] Corrección en validación de input

--- auth/src/routes/sign.routes.ts
+++ auth/src/routes/sign.routes.ts
@@ -1,13 +1,26 @@
 import { Router } from 'express';
+import { validateRequest } from "@hdozdev/common";
+import { body } from 'express-validator';
 import { authController } from "../controllers/index.controllers";
-import { Validator } from '@hdozdev/common'; 
 
 const router = Router();
 
 router.post('/api/users/signout', authController.postsignOut)
 
-router.post('/api/users/signin', Validator.LoginUserValidationResult(), Validator.validate, authController.postsignIn);
+router.post('/api/users/signin', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password must be provide'),
+], validateRequest, authController.postsignIn);
 
-router.post('/api/users/signup', Validator.CreateUserValidationResult(), Validator.validate, authController.postsignUp);
+router.post('/api/users/signup', [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters'),
+  body('name').isLength({min: 1, max: 25}).withMessage('Name must be provided')
+], validateRequest, authController.postsignUp);
 
 export {router as signRouter};
