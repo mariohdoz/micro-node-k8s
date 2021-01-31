b5071950c73b9eccf9891c8a420b6650ff5b4c1c
Mario Ossandón Z <mario.hdoz1@gmail.com>
2021-01-31 15:20:23 -0300

    [MOD] Creación de ticket

--- tickets/src/controllers/ticket.controllers.ts
+++ tickets/src/controllers/ticket.controllers.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from 'express';
+import { Ticket } from "../models/index.models";
 
-const createTicket = (req: Request, res: Response) => {
+const createTicket = async (req: Request, res: Response) => {
   
-  res.sendStatus(200);
+  const {title, price} = req.body;
+  
+  const ticket = Ticket.build({
+    title,
+    price,
+    userId: req.currentUser!.id
+  });
+
+  await ticket.save();
+  
+  res.status(201)
+    .send({
+      ticket
+    });
 }
 
 export {createTicket}
--- tickets/src/routes/createTicket.routes.ts
+++ tickets/src/routes/createTicket.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import { requiereAuth, Validate } from "@hdozdev/common";
+import { body } from "express-validator";
 import { ticketController } from "../controllers/index.controllers";
-import { requiereAuth } from "@hdozdev/common";
 
 const router = Router();
 
-router.post('/api/tickets', requiereAuth, ticketController.createTicket);
+router.post('/api/tickets', requiereAuth, [
+  body('title').not().isEmpty().withMessage('Title is required'),
+  body('price').not().isEmpty(). isFloat({ gt: 0 }).withMessage('Title is required'),
+], Validate.validateRequest, ticketController.createTicket);
 
 export {router as newRouter};
