b7fda9d12f9df44655bdece299bc112721325ed6
Mario Ossandón Z <mario.hdoz1@gmail.com>
2021-01-31 15:18:49 -0300

    [MOD] Actualización paquete

--- auth/src/routes/sign.routes.ts
+++ auth/src/routes/sign.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { validateRequest } from "@hdozdev/common";
+import { Validate } from "@hdozdev/common";
 import { body } from 'express-validator';
 import { authController } from "../controllers/index.controllers";
 
@@ -15,12 +15,12 @@ router.post('/api/users/signin', [
     .trim()
     .notEmpty()
     .withMessage('Password must be provide'),
-], validateRequest, authController.postsignIn);
+], Validate.validateRequest, authController.postsignIn);
 
 router.post('/api/users/signup', [
   body('email').isEmail().withMessage('Email must be valid'),
   body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters'),
   body('name').isLength({min: 1, max: 25}).withMessage('Name must be provided')
-], validateRequest, authController.postsignUp);
+], Validate.validateRequest, authController.postsignUp);
 
 export {router as signRouter};
