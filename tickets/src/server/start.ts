621eec33189d1042eea273a5c35eec49b92c94bb
Mario Ossandón Z <mario.hdoz1@gmail.com>
2021-03-09 22:45:47 -0300

    [ADD] Creación de nats wrapper

--- tickets/src/server/start.ts
+++ tickets/src/server/start.ts
@@ -1,6 +1,7 @@
 import { app } from "./server";
 import mongoose from "mongoose";
 import config from "../config/index";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
 
@@ -10,13 +11,25 @@ const start = async () => {
 
   if(!config.MONGO_URI){
     throw new Error('MONGO_URI debe estar definida');
+  } 
+
+  try {
+    await natsWrapper.connect('ticketing', 'asdasd', 'http://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+  } catch (err) {
+    console.log(err);
   }
 
   try {
     await mongoose.connect(config.MONGO_URI , {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
-    console.log('Base de datos conectada')
+    console.log('Base de datos conectada');
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 
   app.listen(config.PORT, () => {
